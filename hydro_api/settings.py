e268be1e9704193c732ea474ae7552eaee256511
František Macek <frantisek.macek1@gmail.com>
2024-04-24 19:45:31 +0200

    chore: docker setup

--- hydro_api/settings.py
+++ hydro_api/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
+
+env=environ.Env(DEBUG=(bool, False))
+
+environ.Env.read_env(env_file='.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')a#=f^&8!_9+6cynr=qla=oo!vkd$^fk_uyw7k9eew!g-en%3_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -81,8 +87,12 @@ WSGI_APPLICATION = 'hydro_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRESS_PASS'),
+        'HOST': env('PG_HOST'), #TOHLE MOZNA ZMENIT NA LOCALHOST
+        'PORT': env('PG_PORT'),
     }
 }
 
