0e9f4b8e70d8ad3b9ff53ed665995cd603bf83f3
František Macek <frantisek.macek1@gmail.com>
2024-05-19 13:10:06 +0200

    chore: urls configured

--- hydro/urls.py
+++ hydro/urls.py
@@ -1,7 +1,6 @@
 # riverapp/urls.py
 from django.urls import path
-from .views import station_data
-from .views import water_level_chart, get_station_data, get_chart_data, metadata, get_dropdown_values, get_available_years
+from .views import metadata
 
 urlpatterns = [
     path('', metadata, name='dynamic_chart'),
