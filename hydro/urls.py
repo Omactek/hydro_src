ab9a6b3cf9c36cbc80b6a91646f55367414e41dc
František Macek <frantisek.macek1@gmail.com>
2024-05-19 17:04:34 +0200

    created more templates

--- hydro/urls.py
+++ hydro/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import metadata
 
 urlpatterns = [
-    path('', metadata, name='dynamic_chart'),
+    path('', metadata, name='test_template'), #tady je něco špatně
+    path('dynamic_chart/', metadata, name='dynamic_chart'),
 ]
--- hydro/views.py
+++ hydro/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import ValuesMetadata, StationMetadata
+from django.apps import apps
 
-# Create your views here.
+def metadata(request):
+    #retrieve station names and labels from StationMetadata
+    station_metadata = StationMetadata.objects.all()
+    #retrieve values metadata from ValuesMetadata
+    values_metadata = ValuesMetadata.objects.all()
+    list = ["water_level", "air_temperature"]
+    values_metadata = ValuesMetadata.objects.filter(parameter_abreviation_in_data_file__in=list)
+
+    context = {
+        'station_metadata': station_metadata,  #pass station metadata to the template
+        'values_metadata': values_metadata, #pass values metadata to the template
+    }
+
+    return render(request, 'dynamic_chart.html', context)
