d61d9962be0052a3d08276fa91086129353b30d0
František Macek <frantisek.macek1@gmail.com>
2024-06-04 15:47:41 +0200

    urls

--- hydro/urls.py
+++ hydro/urls.py
@@ -1,6 +1,6 @@
 # riverapp/urls.py
 from django.urls import path
-from .views import metadata, ChartDataView, GetAvailableYearsView, MyFormView
+from .views import MyFormView
 
 urlpatterns = [
     path('test_template/', MyFormView.as_view(), name='my_form_view')
